f3367714d5c61d5881ab49f930f6cb2f6aeac1ee
B1uEx <brux@qq.com>
2024-06-18 19:38:13 +0800

    up article

--- source/foto/core-hook.sh
+++ source/foto/core-hook.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # 本脚本用于解决米家无法联网
-# vi /etc/v2raya/mijia-hook.sh
-# chmod +x /usr/share/mijia-hook.sh
+# vi /etc/v2raya/core-hook.sh
+# chmod +x /etc/v2raya/mijia-hook.sh
 # vi /etc/init.d/v2raya
-# 再start_service函数中添加append_env_arg "config" "core_hook" "/usr/share/mijia-hook.sh"
+# 再start_service函数中添加append_env_arg "config" "core_hook" "/etc/v2raya/core-hook.sh"
 echo hook
 jq '.inbounds[] |= if .tag == "transparent" then (.sniffing.domainsExcluded = ["mijia cloud","courier.push.apple.com","dlg.io.mi.com"]) else . end' /etc/v2raya/config.json > /etc/v2raya/temp.json
 mv /etc/v2raya/temp.json /etc/v2raya/config.json
